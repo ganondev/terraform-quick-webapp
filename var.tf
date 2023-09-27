f5718dd245a8e1b3ae564a0258ef196f4fc3ec76
EJ <me@me>
2023-09-27 02:31:08 -0400

    README

--- var.tf
+++ var.tf
@@ -1,24 +1,29 @@
 variable "domain_name" {
   type = string
+  description = "The domain name your webapp will live at. Required for Route53 aliases, as well as the ACM request."
 }
 
 variable "route53_hosted_zone_id" {
   type = string
+  description = "ID for the Route53 hosted zone that created records will be added to"
 }
 
 variable "acm_alternative_names" {
   type = list(string)
   default = []
+  description = "Alternative domain names that the ACM certificate should manage, if any."
 }
 
 variable "site_bucket_name" {
   type = string
   default = null
+  description = "Name of the S3 bucket origin for site content. Will use domain_name by default."
 }
 
 variable cloudfront_aliases {
   type = list(string)
   default = null
+  description = "Any aliases for the cloudfront distribution. By default this just be an array containing domain_name."
 }
 
 locals {
