e3aeb8971c412cacda2f90e547351aef83eecdf4
EJ <me@me>
2023-09-27 21:41:38 -0400

    Create repo from template

--- github.tf
+++ github.tf
@@ -2,7 +2,12 @@ resource github_repository frontend_repository {
 
   name = local.frontend_repository_name
   lifecycle {
-    ignore_changes = "all"
+    ignore_changes = all
+  }
+
+  template {
+    owner      = "ganondev"
+    repository = "quick-webapp-template"
   }
 
 }
